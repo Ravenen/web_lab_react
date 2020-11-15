6dd5987ba6a012acc59f3a2faa9c41a8cc914d80
Raven <redstoyningbros@gmail.com>
2020-11-15 21:10:07 +0200

    Added component for tags

---
+++ src/components/TagsComponent/TagsComponent.js
@@ -0,0 +1,16 @@
+import { Box, Chip } from "@material-ui/core";
+import React from "react";
+
+const TagsComponent = ({ tags }) => {
+  return (
+    <Box display="flex" my={1.5}>
+      {tags.map((tag) => (
+        <Box mx={0.5} component={Chip} label={tag}>
+          {/* <Chip label={tag} /> */}
+        </Box>
+      ))}
+    </Box>
+  );
+};
+
+export default TagsComponent;
