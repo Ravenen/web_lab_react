c5b4d77ed6718c99bb7ed40f3a53599d0a30f47a
Raven <redstoyningbros@gmail.com>
2020-11-15 23:43:06 +0200

    Added custom button with ability to set system color

--- src/components/RoundedButton/RoundedButton.js
+++ src/components/RoundedButton/RoundedButton.js
@@ -1,6 +1,7 @@
 import { Button } from "@material-ui/core";
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import ColoredButton from "../ColoredButton/ColoredButton";
 
 export const useStyles = makeStyles((theme) => ({
   rounded: {
@@ -13,7 +14,7 @@ export const useStyles = makeStyles((theme) => ({
 
 const RoundedButton = (props) => {
   const classes = useStyles();
-  return <Button {...props} className={classes.rounded}></Button>;
+  return <ColoredButton {...props} className={classes.rounded}></ColoredButton>;
 };
 
 export default RoundedButton;
