88472ea12091858c74c08fed2356ad4f7113ccc9
Raven <redstoyningbros@gmail.com>
2020-11-16 02:22:52 +0200

    Added copyright

--- src/containers/Footer/Footer.js
+++ src/containers/Footer/Footer.js
@@ -5,40 +5,55 @@ import IconWrapper from "../../components/IconWrapper/IconWrapper";
 
 const Footer = () => {
   return (
+    // <Box
+    //   component={Grid}
+    //   container
+    //   display="flex"
+    //   justify="space-between"
+    //   alignItems="center"
+    //   px={20}
+    //   py={10}
+    // >
     <Box
-      component={Grid}
-      container
       display="flex"
-      justify="space-between"
+      justifyContent="space-between"
       alignItems="center"
+      flexWrap="wrap"
       px={20}
       py={10}
     >
-      <Grid item xs={4}>
-        <Typography variant="h5">Branding stuff</Typography>
-        <br />
-        <Typography variant="body2" color="textSecondary" component="p">
-          In fugiat pariatur consectetur fugiat ut laborum proident minim
-          occaecat ad nostrud. Occaecat qui mollit in elit pariatur nostrud.
-          Sint officia nostrud non reprehenderit amet et eu aliquip.
-        </Typography>
+      <Grid container justify="space-between" alignItems="center">
+        <Grid item xs={4}>
+          <Typography variant="h5">Branding stuff</Typography>
+          <br />
+          <Typography variant="body2" color="textSecondary" component="p">
+            In fugiat pariatur consectetur fugiat ut laborum proident minim
+            occaecat ad nostrud. Occaecat qui mollit in elit pariatur nostrud.
+            Sint officia nostrud non reprehenderit amet et eu aliquip.
+          </Typography>
+        </Grid>
+        <Box
+          component={Grid}
+          item
+          xs={4}
+          display="flex"
+          justifyContent="flex-end"
+        >
+          <IconWrapper href="https://www.facebook.com/vpavliyk/">
+            <Facebook />
+          </IconWrapper>
+          <IconWrapper href="https://twitter.com/Engenius7">
+            <Twitter />
+          </IconWrapper>
+          <IconWrapper href="https://www.linkedin.com/in/vpavliyk/">
+            <LinkedIn />
+          </IconWrapper>
+        </Box>
       </Grid>
-      <Box
-        component={Grid}
-        item
-        xs={4}
-        display="flex"
-        justifyContent="flex-end"
-      >
-        <IconWrapper href="https://www.facebook.com/vpavliyk/">
-          <Facebook />
-        </IconWrapper>
-        <IconWrapper href="https://twitter.com/Engenius7">
-          <Twitter />
-        </IconWrapper>
-        <IconWrapper href="https://www.linkedin.com/in/vpavliyk/">
-          <LinkedIn />
-        </IconWrapper>
+      <Box mx="auto" mt={4}>
+        <Typography variant="body2" color="textSecondary" component="p">
+          2020 IoT © Copyright all rights reserved, bla bla
+        </Typography>
       </Box>
     </Box>
   );
