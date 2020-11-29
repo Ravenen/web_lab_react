939377203ff36038601345b6d6cb50a7ef860605
Raven <redstoyningbros@gmail.com>
2020-11-29 15:12:49 +0200

    Added image and description

--- src/containers/ItemPage/ItemPage.js
+++ src/containers/ItemPage/ItemPage.js
@@ -1,9 +1,31 @@
+import { Box } from "@material-ui/core";
 import React from "react";
 import { useParams } from "react-router-dom";
+import ItemDescription from "../../components/ItemDescription/ItemDescription";
+import { API, productImages } from "../App/Utils";
 
 const ItemPage = () => {
   const { id } = useParams();
-  return <div>{id}</div>;
+  const item = API.getById(parseInt(id));
+  console.log(item);
+  return (
+    <Box mx={20} pt={20}>
+      <Box display="flex" mx="auto" alignItems="flex-start">
+        <Box
+          component="img"
+          alt="hero"
+          src={productImages[id] || productImages[0]}
+          // height="80%"
+          width="30%"
+          ml={10}
+          // mr={15}
+        />
+        <Box px={15}>
+          <ItemDescription {...item} />
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default ItemPage;
