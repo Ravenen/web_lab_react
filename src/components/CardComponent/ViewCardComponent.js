1fb7766cac8d8e640a471e61fada4e0bd8537cc1
Raven <redstoyningbros@gmail.com>
2020-12-06 19:44:41 +0200

    Remade logic to use items from global context. Added sceleton loading.

--- src/components/CardComponent/ViewCardComponent.js
+++ src/components/CardComponent/ViewCardComponent.js
@@ -9,6 +9,7 @@ import {
 import React from "react";
 import { StyledBadge } from "../StyledBadge/StyledBadge.styled";
 import { productImages } from "../../utils/Utils";
+import { Skeleton } from "@material-ui/lab";
 
 const useStyles = makeStyles({
   media: {
@@ -18,32 +19,49 @@ const useStyles = makeStyles({
 
 const ViewCardComponent = (props) => {
   const classes = useStyles();
-
+  const isLoading = props.isLoading || false;
   return (
     <Card>
-      <CardMedia
-        className={classes.media}
-        image={productImages[props.id] || productImages[0]}
-        title="Garland"
-      />
+      {isLoading ? (
+        <Skeleton variant="rect" height={360} />
+      ) : (
+        <CardMedia
+          className={classes.media}
+          image={productImages[props.id] || productImages[0]}
+          title="Garland"
+        />
+      )}
       <CardContent>
         <Box textAlign="center" mb={2}>
-          <Typography gutterBottom variant="h5" component="h2">
-            <StyledBadge
-              badgeContent={props.id % 3 === 0 ? "HOT" : 0}
-              color="primary"
-            >
-              Garland
-            </StyledBadge>
-          </Typography>
+          {isLoading ? (
+            <Box mx={10}>
+              <Skeleton />
+            </Box>
+          ) : (
+            <Typography gutterBottom variant="h5" component="h2">
+              <StyledBadge
+                badgeContent={props.id % 3 === 0 ? "HOT" : 0}
+                color="primary"
+              >
+                Garland
+              </StyledBadge>
+            </Typography>
+          )}
         </Box>
         <Box textAlign="center">
-          <Typography variant="body2" color="textSecondary">
-            <Box lineHeight={2} component="span">
-              Quis culpa deserunt do quis commodo nulla. Aute non aliquip est
-              aliquip do incididunt irure duis anim ullamco.
+          {isLoading ? (
+            <Box>
+              <Skeleton />
+              <Skeleton />
             </Box>
-          </Typography>
+          ) : (
+            <Typography variant="body2" color="textSecondary">
+              <Box lineHeight={2} component="span">
+                Quis culpa deserunt do quis commodo nulla. Aute non aliquip est
+                aliquip do incididunt irure duis anim ullamco.
+              </Box>
+            </Typography>
+          )}
         </Box>
       </CardContent>
     </Card>
--- src/containers/Home/LatestProducts/LatestProducts.js
+++ src/containers/Home/LatestProducts/LatestProducts.js
@@ -1,11 +1,14 @@
 import { Box, Divider, Typography } from "@material-ui/core";
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import ViewCardComponent from "../../../components/CardComponent/ViewCardComponent";
 import RoundedButton from "../../../components/RoundedButton/RoundedButton";
 import CardGridContainer from "../../../components/CardGrid/CardGridContainer";
 import CardGridWrapper from "../../../components/CardGrid/CardGridWrapper";
 import { StyledToggleLink } from "./LatestProducts.styled";
-import { ItemsContext } from "../../../utils/Contexts";
+import { useGlobalContext } from "../../../utils/Contexts";
+import { getAllGarlands } from "../../../utils/API";
+import { INIT_GARLANDS } from "../../../utils/ActionTypes";
+import ProductsGrid from "./ProductsGrid/ProductsGrid";
 
 const seeMoreText = {
   seeMore: "See more",
@@ -13,21 +16,37 @@ const seeMoreText = {
 };
 
 const LatestProducts = () => {
-  const { allItems } = useContext(ItemsContext);
+  const { garlands, dispatch } = useGlobalContext();
+  const [isLoading, setLoading] = useState(true);
   const [naturalFilter, setNaturalFiler] = useState(0);
   const [showAllItems, setShowAllItems] = useState(false);
   const [seeMoreButtonText, setSeeMoreButtonText] = useState(
     seeMoreText.seeMore
   );
-  const [itemsToShow, setItemsToShow] = useState(allItems.slice(-3).reverse());
+  const [itemsToShow, setItemsToShow] = useState(garlands.slice(-3).reverse());
+
+  useEffect(() => {
+    setTimeout(() => {
+      getAllGarlands()
+        .then((res) => {
+          dispatch({ type: INIT_GARLANDS, payload: res.data });
+        })
+        .catch((e) => console.log(e))
+        .finally(() => setLoading(false));
+    }, 3000);
+  }, [dispatch]);
+  useEffect(() => {
+    setItemsToShow(garlands);
+  }, [garlands]);
+
   const handleSeeMore = () => {
     let nextShowAllItems = !showAllItems;
     if (nextShowAllItems) {
-      let reversedItems = allItems.slice().reverse();
+      let reversedItems = itemsToShow.slice().reverse();
       setItemsToShow(reversedItems);
       setSeeMoreButtonText(seeMoreText.seeLess);
     } else {
-      let lastItems = allItems.slice(-3).reverse();
+      let lastItems = itemsToShow.slice(-3).reverse();
       setItemsToShow(lastItems);
       setSeeMoreButtonText(seeMoreText.seeMore);
     }
@@ -60,24 +79,19 @@ const LatestProducts = () => {
         <Divider />
       </Box>
       <CardGridContainer>
-        {itemsToShow.map((garland) => (
-          <CardGridWrapper key={garland.id}>
-            <ViewCardComponent
-              key={garland.id}
-              {...garland}
-            ></ViewCardComponent>
-          </CardGridWrapper>
-        ))}
+        <ProductsGrid garlands={itemsToShow} isLoading={isLoading} />
       </CardGridContainer>
-      <Box mx="auto" mt={10}>
-        <RoundedButton
-          color="secondary"
-          variant="contained"
-          onClick={handleSeeMore}
-        >
-          {seeMoreButtonText}
-        </RoundedButton>
-      </Box>
+      {!isLoading && (
+        <Box mx="auto" mt={10}>
+          <RoundedButton
+            color="secondary"
+            variant="contained"
+            onClick={handleSeeMore}
+          >
+            {seeMoreButtonText}
+          </RoundedButton>
+        </Box>
+      )}
     </Box>
   );
 };
