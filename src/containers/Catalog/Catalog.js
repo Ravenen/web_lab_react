3d03f1e9d9a9ebfc9e269744a585e033f253de3a
Raven <redstoyningbros@gmail.com>
2020-12-12 21:30:02 +0200

    Moved header

--- src/containers/Catalog/Catalog.js
+++ src/containers/Catalog/Catalog.js
@@ -1,11 +1,12 @@
 import React from "react";
+import PageHeader from "../../components/PageHeader/PageHeader";
 import CatalogHeader from "./CatalogHeader/CatalogHeader";
 import ProductList from "./ProductList/ProductList";
 
 const Catalog = () => {
   return (
     <div>
-      <CatalogHeader />
+      <PageHeader>Product Catalog</PageHeader>
       <ProductList />
     </div>
   );
