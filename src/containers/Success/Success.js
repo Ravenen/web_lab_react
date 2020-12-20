1ba78f7b048f47b1da18bdde5eb73f3c816e6ba2
Raven <redstoyningbros@gmail.com>
2020-12-20 18:19:41 +0200

    Designed success page

--- src/containers/Success/Success.js
+++ src/containers/Success/Success.js
@@ -1,7 +1,34 @@
+import { Box, Typography } from "@material-ui/core";
+import { Alert, AlertTitle } from "@material-ui/lab";
 import React from "react";
+import { HoverableNavLink } from "../../components/HoverableLink/HoverableLink.styled";
+import { links } from "../../utils/Utils";
 
 const Success = () => {
-  return <div></div>;
+  return (
+    <Box mx={20} pt={15}>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Box component={Typography} variant="h3" py={5}>
+          Success!
+        </Box>
+        <Alert severity="success">
+          <AlertTitle>Success!</AlertTitle>
+          Wow! Amazing! You did it! Aren't you tired? Hope you're proud of
+          yourself, jerk
+        </Alert>
+        <Box mt={10}>
+          <Box component={Typography} variant="body1" textAlign="center">
+            <HoverableNavLink
+              to={links.catalog}
+              style={{ textDecoration: "underline" }}
+            >
+              Go Back to Catalog
+            </HoverableNavLink>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Success;
