e813fb53c027bd0d9366ea48c0e695e1db3c6f3a
Raven <redstoyningbros@gmail.com>
2020-12-20 18:00:42 +0200

    Added fields for form

---
+++ src/components/CustomInputField/CustomInputField.js
@@ -0,0 +1,22 @@
+import { TextField } from "@material-ui/core";
+import { useField } from "formik";
+import React from "react";
+
+const CustomInputField = ({ id, name, ...props }) => {
+  const [field, meta] = useField(name);
+  const isError = meta.touched && meta.error;
+  const errorText = isError ? meta.error : "";
+  return (
+    <TextField
+      id={id}
+      name={name}
+      {...field}
+      {...props}
+      error={!!isError}
+      helperText={errorText || props.helperText}
+      variant="outlined"
+    />
+  );
+};
+
+export default CustomInputField;
--- src/containers/Chechkout/Checkout.js
+++ src/containers/Chechkout/Checkout.js
@@ -1,9 +1,11 @@
 import { Box, Typography } from "@material-ui/core";
-import { Formik } from "formik";
+import { Form, Formik } from "formik";
 import React from "react";
-import GoBack from "../../components/GoBack/GoBack";
-import { links } from "../../utils/Utils";
+import { useHistory } from "react-router-dom";
 import * as Yup from "yup";
+import ColoredButton from "../../components/ColoredButton/ColoredButton";
+import CustomInputField from "../../components/CustomInputField/CustomInputField";
+import GoBack from "../../components/GoBack/GoBack";
 
 const initialValues = {
   firstName: "",
@@ -36,15 +38,96 @@ const validationSchema = Yup.object({
 });
 
 const Checkout = () => {
+  const history = useHistory();
+  const handleSubmit = (values, { setSubmitting }) => {
+    setSubmitting(true);
+    console.log(values);
+    setTimeout(() => {
+      setSubmitting(false);
+      history.push("/success");
+    }, 3000);
+  };
   return (
     <Box mx={20} pt={15}>
-      <GoBack to={links.cart} />
+      <GoBack />
       <Box display="flex" flexDirection="column" alignItems="center">
         <Typography variant="h3">Checkout</Typography>
         <Formik
           initialValues={initialValues}
           validationSchema={validationSchema}
-        ></Formik>
+          onSubmit={handleSubmit}
+        >
+          {({ isSubmitting }) => (
+            <Box
+              component={Form}
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+            >
+              <Box display="flex" justifyContent="space-between" mt={5}>
+                <Box mr={4}>
+                  <CustomInputField
+                    id="firstName"
+                    name="firstName"
+                    label="First name"
+                    type="text"
+                    required
+                  />
+                </Box>
+                <Box ml={4}>
+                  <CustomInputField
+                    id="lastName"
+                    name="lastName"
+                    label="Last name"
+                    type="text"
+                    required
+                  />
+                </Box>
+              </Box>
+              <Box display="flex" justifyContent="space-between" mt={5}>
+                <Box mr={4}>
+                  <CustomInputField
+                    id="age"
+                    name="age"
+                    label="Age"
+                    type="number"
+                    required
+                  />
+                </Box>
+                <Box ml={4}>
+                  <CustomInputField
+                    id="email"
+                    name="email"
+                    label="Email"
+                    type="email"
+                    required
+                  />
+                </Box>
+              </Box>
+              <Box display="flex" justifyContent="space-between" mt={5}>
+                <Box>
+                  <CustomInputField
+                    id="phoneNumber"
+                    name="phoneNumber"
+                    label="Phone number"
+                    type="tel"
+                    helperText="(optional)"
+                  />
+                </Box>
+              </Box>
+              <Box mt={5}>
+                <ColoredButton
+                  color="success"
+                  disabled={isSubmitting}
+                  variant="contained"
+                  type="submit"
+                >
+                  Submit
+                </ColoredButton>
+              </Box>
+            </Box>
+          )}
+        </Formik>
       </Box>
     </Box>
   );
