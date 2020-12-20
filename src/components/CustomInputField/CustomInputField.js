d741444dab6cdb26afce0e1054bafb65cceeb4f1
Raven <redstoyningbros@gmail.com>
2020-12-20 18:34:28 +0200

    Fixed variable logic

--- src/components/CustomInputField/CustomInputField.js
+++ src/components/CustomInputField/CustomInputField.js
@@ -4,15 +4,16 @@ import React from "react";
 
 const CustomInputField = ({ id, name, ...props }) => {
   const [field, meta] = useField(name);
-  const isError = meta.touched && meta.error;
+  const isError = !!(meta.touched && meta.error);
   const errorText = isError ? meta.error : "";
+  console.log(meta);
   return (
     <TextField
       id={id}
       name={name}
       {...field}
       {...props}
-      error={!!isError}
+      error={isError}
       helperText={errorText || props.helperText}
       variant="outlined"
     />
